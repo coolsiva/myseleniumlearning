23a082d1acc9615e40afdd53847c6178f7696248
coolsiva <coolsiva1990@gmail.com>
2020-06-16 19:10:49 +0530

    prime py

---
+++ Prime.py
@@ -0,0 +1,14 @@
+n =int(input('enter a number'))
+count=0
+for i in range(3,n):
+    for j  in range(2,i):
+        isprime = True
+        if(i%j==0):
+            isprime = False
+            break
+    if(isprime):
+        print(i)
+        count=count+1
+        if(count==5):
+            exit()
+    
